63a0e923c0bd0cebb4434ca2f23d381f8c49dc5e
mfrFrabbi <mdfazlerabbi.diu@gmail.com>
2021-01-27 20:43:39 +0600

    JlayerdPane

--- PracticesPro/src/com/company/Main.java
+++ PracticesPro/src/com/company/Main.java
@@ -9,66 +9,45 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         JFrame f = new JFrame();
-
-        f.setTitle("Border+Pannel");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setLayout(new BorderLayout());
         f.setSize(500,500);
-        f.setVisible(true);
+        f.setTitle("JLayerdPane");
+        f.setLayout(null);
 
+        JLayeredPane layeredPane = new JDesktopPane();
+        layeredPane.setSize(500,500);
+        layeredPane.setBackground(Color.DARK_GRAY);
         JLabel label = new JLabel();
-        label.setText("Rabbi");
+        JLabel label2 = new JLabel();
+        JLabel label3 = new JLabel();
+        JLabel label4 = new JLabel();
 
-        JPanel p1 = new JPanel();
-        JPanel p2 = new JPanel();
-        JPanel p3 = new JPanel();
-        JPanel p4 = new JPanel();
-        JPanel p5 = new JPanel();
+        label.setBounds(100,100,200,200);
+        label.setOpaque(true);
+        label.setBackground(Color.pink);
 
-        p1.setBackground(Color.pink);
-        p1.add(label);
-        p2.setBackground(Color.DARK_GRAY);
-        p3.setBackground(Color.DARK_GRAY);
-        p4.setBackground(Color.magenta);
-        p5.setBackground(Color.blue);
+        label2.setBounds(150,150,200,200);
+        label2.setOpaque(true);
+        label2.setBackground(Color.green);
 
-        p1.setPreferredSize(new Dimension(100,100));
-        p2.setPreferredSize(new Dimension(100,100));
-        p3.setPreferredSize(new Dimension(100,100));
-        p4.setPreferredSize(new Dimension(100,100));
-        p5.setPreferredSize(new Dimension(100,100));
+        label3.setBounds(200,200,200,200);
+        label3.setOpaque(true);
+        label3.setBackground(Color.red);
 
-        f.add(p1,BorderLayout.NORTH);
-        f.add(p2,BorderLayout.WEST);
-        f.add(p3,BorderLayout.EAST);
-        f.add(p4,BorderLayout.SOUTH);
-        f.add(p5,BorderLayout.CENTER);
-
-        p5.setLayout(new BorderLayout());
-
-        JPanel p6 = new JPanel();
-        JPanel p7 = new JPanel();
-        JPanel p8 = new JPanel();
-        JPanel p9 = new JPanel();
-
-        p6.setBackground(Color.green);
-        p7.setBackground(Color.green);
-        p8.setBackground(Color.green);
-        p9.setBackground(Color.green);
+        label4.setBounds(250,250,200,200);
+        label4.setOpaque(true);
+        label4.setBackground(Color.blue);
 
 
 
-        p6.setPreferredSize(new Dimension(50,50));
-        p7.setPreferredSize(new Dimension(50,50));
-        p8.setPreferredSize(new Dimension(50,50));
-        p9.setPreferredSize(new Dimension(50,50));
+        layeredPane.add(label);
+        layeredPane.add(label2);
+        layeredPane.add(label3);
+        layeredPane.add(label4);
 
 
-
-        p5.add(p6,BorderLayout.EAST);
-        p5.add(p7,BorderLayout.WEST);
-        p5.add(p8,BorderLayout.NORTH);
-        p5.add(p9,BorderLayout.SOUTH);
+        f.add(layeredPane);
+        f.setVisible(true);
 
 
 
